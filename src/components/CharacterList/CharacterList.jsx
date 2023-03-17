6b6f65bc09bb24a767bdd9d2519c890c40185bf1
Dima Telechkun <telechkun93@gmail.com>
2023-03-17 21:43:19 +0200

    character page layout done

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import rick from '../../assets/Rick.png';
+import css from './CharacterList.module.css';
 
 function CharacterList() {
-  return <div>CharacterList</div>;
+  return (
+    <>
+      <ul>
+        {/* {Array.isArray(movieCast) &&
+          movieCast?.map(({ id, name, profile_path }) => {
+            return ( */}
+        <li
+          // key={id}
+          className={css.cardStyle}
+        >
+          <img
+            // className={css.imageStyle}
+            src={
+              rick
+              // ? { rick }
+              // : 'https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png'
+            }
+            alt="name"
+          />
+          <p className={css.titleStyle}>Rick Sanchez</p>
+          <p className={css.specieStyle}>Human</p>
+        </li>
+        {/* );
+          })} */}
+      </ul>
+    </>
+  );
 }
 
 export default CharacterList;
--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import person from '../../assets/RickSan.png';
+import css from './CharacterPage.module.css';
 
 function CharacterPage() {
-  return <div>CharacterPage</div>;
+  return (
+    <div>
+      <div className={css.styleCharacter}>
+        <img src={person} alt="person" className={css.styleCover} />
+        <p className={css.styleName}>Rick Sanchez</p>
+      </div>
+      <div className={css.infoCard}>
+        <p className={css.styleInfo}>Information</p>
+        <div className={css.styleItem}>
+          <p className={css.styleMain}>Gender</p>
+          <p className={css.styleSecondary}>Male</p>
+        </div>
+        <div className={css.styleItem}>
+          <p className={css.styleMain}>Status</p>
+          <p className={css.styleSecondary}>Alive</p>
+        </div>
+        <div className={css.styleItem}>
+          <p className={css.styleMain}>Specie</p>
+          <p className={css.styleSecondary}>Human</p>
+        </div>
+        <div className={css.styleItem}>
+          <p className={css.styleMain}>Origin</p>
+          <p className={css.styleSecondary}>Earth</p>
+        </div>
+        <div className={css.styleItem}>
+          <p className={css.styleMain}>Type</p>
+          <p className={css.styleSecondary}>Unknown</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CharacterPage;
